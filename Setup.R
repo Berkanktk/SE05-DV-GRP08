ecbe212389d1e790abe87f743a22eacfc6e703de
Berkan Kütük <bekut20@student.sdu.dk>
2022-10-10 13:31:46 +0200

    Added setup file

---
+++ Setup.R
@@ -0,0 +1,3 @@
+pkgs <- c("tidyverse", "ggplot2", "dplyr", "tidyr","mosaicData", "carData","VIM", "scales", "treemapify","gapminder", "ggmap", "choroplethr","choroplethrMaps", "CGPfunctions","ggcorrplot", "visreg","gcookbook", "forcats","survival", "survminer","ggalluvial", "ggridges","GGally", "superheat","waterfalls", "factoextra","networkD3", "ggthemes","hrbrthemes", "ggpol","ggbeeswarm")
+
+install.packages(pkgs)
