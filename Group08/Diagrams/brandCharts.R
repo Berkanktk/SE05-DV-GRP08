62b8f10b9942ce9cc17f90fc24ce6946f5d7c1aa
Da9el00 <danielsmidstrup1@gmail.com>
2022-11-13 19:07:12 +0100

    brand bar charts2

---
+++ Group08/Diagrams/brandCharts.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(ggplot2)
+library(gganimate)
+dat <- read_csv("Data_Only_Year.csv")
+
+#Brand
+datBrand <- dat %>% select(c("Brand", "Release_Date"))
+datBrand <- drop_na(datBrand)
+
+brandCount <- datBrand %>%
+  count(Brand)
+
+#barplot(brandCount$n)
+
+#Large Brands
+brandCount <- brandCount %>%
+  filter(n >= 35)
+
+#ggplot(data=brandCount, aes(x=Brand, y=n, fill=Brand)) +
+#  geom_bar(stat="identity", width=0.5)
+
+brandCountByYear <- datBrand %>%
+  count(Brand, Release_Date)
+
+sortedData <- data.frame(Brand=character(), Release_Date=integer(), n=integer())
+
+i = 1
+for(i in 1:nrow(brandCountByYear) ) {
+  j = 1
+  for(j in 1:nrow(brandCount)){
+    if(brandCountByYear$Brand[i] == brandCount$Brand[j]){
+      sortedData[nrow(sortedData) + 1,] <- brandCountByYear[i,]
+    }
+  }
+}
+
+#Animate
+ggplot(sortedData, aes(x=Brand, y=n, fill=Brand)) + 
+  geom_bar(stat='identity') +
+  theme_bw() +
+  labs(title="Phones sold each year by brand",
+       x = "Brands",
+       y = "Amount") +
+  coord_flip() + 
+  # gganimate specific bits:
+  transition_states(
+    Release_Date,
+    transition_length = 2,
+    state_length = 3
+  ) +
+  labs(subtitle="Year:  {closest_state}")+
+  ease_aes('sine-in-out')
+
+# Save at gif:
+anim_save("288-animated-barplot-transition.gif_color")
