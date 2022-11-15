d6221f4878dcb8e1863bd86c2933f9b7d1ac9f3d
Victor Boye <vandreasboye@gmail.com>
2022-11-15 15:40:23 +0100

    Updated phone release date heat map

--- Group08/Diagrams/CalenderHeatMap.R
+++ Group08/Diagrams/CalenderHeatMap.R
@@ -1,11 +1,7 @@
 library(tidyverse)
 library(ggplot2)
-library(gganimate)
 library(zoo)
-library(sqldf)
-library(quantmod)
 library(reshape2)
-library(plyr)
 library(scales)
 library(RColorBrewer)
 library(plotly)
@@ -17,36 +13,21 @@ dat[4] <- dat_dates[1]
 dat <- dat %>% select(c("Brand", "Release_Date"))
 dat <- drop_na(dat)
 
-dat <- dat %>% count("Release_Date")
+dat <- dat %>% count(Release_Date)
 
 dat$year<-as.numeric(as.POSIXlt(dat$Release_Date)$year+1900)
 dat$month<-as.numeric(as.POSIXlt(dat$Release_Date)$mon+1)
 
 dat$monthf<-factor(dat$month,levels=as.character(1:12),labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),ordered=TRUE)
 
-dat$weekday = as.POSIXlt(dat$Release_Date)$wday
-
-dat$weekdayf<-factor(dat$weekday,levels=rev(0:6),labels=rev(c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")),ordered=TRUE)
-
-dat$yearmonth<-as.yearmon(dat$Release_Date)
-
-dat$yearmonthf<-factor(dat$yearmonth)
-
-dat$week <- as.numeric(format(dat$Release_Date,"%W"))
-
 dat$date <- as.numeric(format(dat$Release_Date,"%d"))
 
+query <- dat %>% filter(year >= 2003) %>% filter(year <= 2022) %>% select(c(date, monthf, n))
+grouped <- query %>% group_by(monthf, date) %>% summarise(n=sum(n))
 
-dat<-ddply(dat,.(yearmonthf),transform,monthweek=1+week-min(week))
-
-query = sqldf("SELECT * FROM dat WHERE year BETWEEN 2010 and 2010")
-
-p <- ggplot(query, aes(monthf, date, fill = freq)) + 
+p <- ggplot(grouped, aes(monthf, date, fill = n)) + 
   geom_tile(colour = "white") + 
-  #coord_equal() +
-  #coord_flip() +
-  geom_text(aes(label = round(freq, 1))) +
-  scale_fill_gradient(low="gray", high="red") #+
-  ggtitle("Phone Release Dates") +  xlab("\n Month") + ylab("Dates")
+  scale_fill_gradient(low="gray", high="red") +
+  ggtitle("Phone Release Dates") +  xlab("\nMonth") + ylab("Dates")
   
 ggplotly(p)
